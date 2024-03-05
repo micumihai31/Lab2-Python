cf33728a4fab78bdbb607adcec51ca7ae729a299
IosuaPallo <palloiosua@yahoo.com>
2024-03-05 08:40:24 +0200

    Initial Commit

---
+++ Lab2.py
@@ -0,0 +1,15 @@
+class Animal:
+    def mananca(self):
+        print("Animal")
+
+class Pisica(Animal):
+    def mananca(self):
+        print("Pisica")
+
+#
+animal=Animal()
+animal.mananca()
+
+
+
+
